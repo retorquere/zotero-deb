779dd1441138310db1e548445fcff69d955c7d29
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-11-16 14:00:19 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,15 @@ case `uname -m` in
 esac
 
 if [ -x "$(command -v curl)" ]; then
-  curl --silent -L https://github.com/retorquere/zotero-deb/releases/download/apt-get/deb.gpg.key | gpg --no-default-keyring --keyring /etc/apt/trusted.gpg --import -
+  curl --silent -L https://github.com/retorquere/zotero-deb/releases/download/apt-get/deb.gpg.key | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/zotero.gpg --import -
 elif [ -x "$(command -v wget)" ]; then
-  wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/deb.gpg.key | gpg --no-default-keyring --keyring /etc/apt/trusted.gpg --import -
+  wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/deb.gpg.key | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/zotero.gpg --import -
 else
   echo "Error: need wget or curl installed." >&2
   exit 1
 fi
+
+sudo chmod 644 /etc/apt/trusted.gpg.d/zotero.gpg
 
 cat << EOF | sudo tee /etc/apt/sources.list.d/zotero.list
 deb https://github.com/retorquere/zotero-deb/releases/download/apt-get/ ./
