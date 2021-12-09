2a4d90866f39e9c5b6eb79c3678e441505d35c57
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-12-09 14:49:21 +0100

    relative paths

--- integrated/build.py
+++ integrated/build.py
@@ -10,42 +10,59 @@ import argparse
 import re
 import filetype
 import magic
+import contextlib
 
 args = argparse.ArgumentParser(description='update Zotero deb repo.')
-args.add_argument('--base', type=str, default='.')
-args.add_argument('--config', type=str)
-args.add_argument('--mime', type=str)
+args.add_argument('--root', type=str, default='.')
+args.add_argument('--config', type=str, default='config.ini')
+args.add_argument('--mime', type=str, default='mime.xml')
 args.add_argument('source', nargs='+')
 args = args.parse_args()
+args.root = os.path.abspath(args.root)
 
-class IniFile():
-  def __init__(self, path):
-    self.ini = configparser.RawConfigParser()
-    self.ini.optionxform=str
-    self.ini.read(path)
+@contextlib.contextmanager
+def IniFile(path):
+  ini = configparser.RawConfigParser()
+  ini.optionxform=str
+  ini.read(path)
+  yield ini
+
+@contextlib.contextmanager
+def chdir(path):
+  cwd= os.getcwd()
+  try:
+    print('changing to', path)
+    os.chdir(path)
+    yield
+  finally:
+    print('changing back to', cwd)
+    os.chdir(cwd)
+
+class Open():
+  def __init__(self, path, mode='r', fmode=None):
+    self.path = path
+    if 'w' in mode or 'a' in mode: os.makedirs(os.path.dirname(self.path), exist_ok=True)
+    self.mode = fmode
+    self.f = open(self.path, mode)
 
   def __enter__(self):
-    return self.ini
+    return self.f
 
   def __exit__(self, exc_type, exc_value, exc_traceback):
-    pass
+    self.f.close()
+    if self.mode is not None:
+      os.chmod(self.path, self.mode)
 
 config = types.SimpleNamespace()
-config.base = os.path.abspath(args.base)
-config.mime = args.mime or 'mime.xml'
 
 # load build config
-with IniFile(args.config or 'config.ini') as ini:
+with IniFile(args.config) as ini:
   config.ini = ini
 config.maintainer = config.ini['maintainer']['email']
 config.gpgkey = config.ini['maintainer']['gpgkey']
 
 # remove trailing slash since it messes with basename
 config.source = [ re.sub(r'/$', '', source) for source in args.source ]
-
-# make repo directory
-config.repo = os.path.join(config.base, 'release/apt')
-os.makedirs(config.repo, exist_ok=True)
 
 for source in config.source:
   assert os.path.isdir(source)
@@ -65,14 +82,14 @@ for source in config.source:
     print('created temporary directory', builddir)
     deb.build = builddir
 
-  # get version and binary name
+  # get version, binary name, and base dir under args.root
   with IniFile(os.path.join(source, 'application.ini')) as ini:
     deb.version = ini['App']['Version']
     if '-beta' in deb.version:
-      deb.base = 'beta'
+      deb.dir = 'beta'
       deb.binary = 'zotero-beta'
     else:
-      deb.base = 'release'
+      deb.dir = 'release'
       deb.binary = 'zotero'
 
   deb.bump = ''
@@ -91,7 +108,7 @@ for source in config.source:
     deb.dependencies.append(dep)
   deb.dependencies = ', '.join(sorted(list(set(deb.dependencies))))
   deb.description = config.ini['deb']['description']
-  deb.deb = os.path.join(config.base, deb.base, deb.version, f'{deb.binary}_{deb.version}{deb.bump}_{deb.arch}.deb')
+  deb.deb = os.path.join(args.root, deb.dir, deb.version, f'{deb.binary}_{deb.version}{deb.bump}_{deb.arch}.deb')
 
   # copy zotero to the build directory, excluding the desktpo file (which we'll recreate later) and the update files
   os.makedirs(os.path.join(deb.build, 'usr/lib'), exist_ok=True)
@@ -100,23 +117,9 @@ for source in config.source:
     # rename the 'zotero' binary to 'zotero-beta' for the beta package so they can be installed alongside each other
     shutil.move(os.path.join(deb.build, 'usr/lib', deb.binary, 'zotero'), os.path.join(deb.build, 'usr/lib', deb.binary, deb.binary))
 
-  class Open():
-    def __init__(self, path, mode='r', fmode=None):
-      self.path = os.path.join(deb.build, path)
-      if 'w' in mode or 'a' in mode: os.makedirs(os.path.dirname(self.path), exist_ok=True)
-      self.mode = fmode
-      self.f = open(self.path, mode)
-
-    def __enter__(self):
-      return self.f
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-      self.f.close()
-      if self.mode is not None:
-        os.chmod(self.path, self.mode)
 
   # disable auto-update
-  with Open(f'usr/lib/{deb.binary}/defaults/pref/local-settings.js', 'a') as ls, Open(f'usr/lib/{deb.binary}/mozilla.cfg', 'a') as cfg:
+  with Open(os.path.join(deb.build, 'usr/lib/', deb.binary, 'defaults/pref/local-settings.js'), 'a') as ls, Open(os.path.join(deb.build, 'usr/lib/', deb.binary, 'mozilla.cfg'), 'a') as cfg:
     # enable mozilla.cfg
     if ls.tell() != 0: print('', file=ls)
     print('pref("general.config.obscure_value", 0); // only needed if you do not want to obscure the content with ROT-13', file=ls)
@@ -150,15 +153,15 @@ for source in config.source:
       'application/vnd.citationstyles.style+xml'
     ]))
     ini.set('Desktop Entry', 'Description', deb.description)
-    with Open(f'usr/share/applications/{deb.binary}.desktop', 'w') as f:
+    with Open(os.path.join(deb.build, 'usr/share/applications/{deb.binary}.desktop'), 'w') as f:
       ini.write(f, space_around_delimiters=False)
 
   # add mime info
-  with open(config.mime) as mime, Open(f'usr/share/mime/packages/{deb.binary}.xml', 'w') as f:
+  with open(args.mime) as mime, Open(os.path.join(deb.build, 'usr/share/mime/packages', f'{deb.binary}.xml'), 'w') as f:
     f.write(mime.read())
 
   #write build control file
-  with Open('DEBIAN/control', 'w') as f:
+  with Open(os.path.join(deb.build, 'DEBIAN/control'), 'w') as f:
     print(f'Package: {deb.binary}', file=f)
     print(f'Architecture: {deb.arch}', file=f)
     print(f'Depends: {deb.dependencies}', file=f)
@@ -183,17 +186,15 @@ for source in config.source:
   run(f'fakeroot dpkg-deb --build -Zgzip {shlex.quote(deb.build)} {shlex.quote(deb.deb)}')
   run(f'dpkg-sig -k {shlex.quote(config.gpgkey)} --sign builder {shlex.quote(deb.deb)}')
 
-  # rebuild repo
-  repo = shlex.quote(config.repo)
+# rebuild repo
+config.repo = os.path.abspath(os.path.join(args.root, 'apt'))
+os.makedirs(config.repo, exist_ok=True)
+with chdir(config.repo):
   gpgkey = shlex.quote(config.gpgkey)
-  deb_gpg_key = shlex.quote(os.path.join(config.repo, 'deb.gpg.key'))
-  packages = shlex.quote(os.path.join(config.repo, "Packages"))
-  release = shlex.quote(os.path.join(config.repo, "Release"))
-  release_gpg = shlex.quote(os.path.join(config.repo, "Release.gpg"))
-  inrelease = shlex.quote(os.path.join(config.repo, "InRelease"))
-  run(f'gpg --armor --export {gpgkey} > {deb_gpg_key}')
-  run(f'cd {repo} && apt-ftparchive packages . > {packages}')
-  run(f'bzip2 -kf {packages}')
-  run(f'cd {repo} && apt-ftparchive release . > Release')
-  run(f'gpg --yes -abs -u {gpgkey} -o {release_gpg} --digest-algo sha256 {release}')
-  run(f'gpg --yes -abs -u {gpgkey} --clearsign -o {inrelease} --digest-algo sha256 {release}')
+  relpath = shlex.quote(os.path.relpath(os.path.commonpath([args.root, config.repo]), config.repo))
+  run(f'apt-ftparchive packages {relpath} > Packages')
+  run(f'apt-ftparchive release . > Release')
+  run(f'gpg --armor --export {gpgkey} > deb.gpg.key')
+  run(f'bzip2 -kf Packages')
+  run(f'gpg --yes -abs -u {gpgkey} -o Release.gpg --digest-algo sha256 Release')
+  run(f'gpg --yes -abs -u {gpgkey} --clearsign -o InRelease --digest-algo sha256 Release')
