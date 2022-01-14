359e9b1926a4f5fe2cae1878f7ea793eb45dc74d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2022-01-14 19:52:53 +0100

    b2

--- rebuild.py
+++ rebuild.py
@@ -22,6 +22,7 @@ import html
 
 import b2sdk.v2 as b2
 import multiprocessing
+import time
 
 headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36' }
 import urllib.request
