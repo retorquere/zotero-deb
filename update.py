0b825a8e510a8de44fcb99abf96f87214bc434a5
Emiliano Heyns <emiliano.heyns@han.nl>
2022-03-23 20:13:38 +0100

    update also if install.sh is missing

--- update.py
+++ update.py
@@ -19,14 +19,15 @@ update = '_true_' in UPDATE
 if not update:
   request = Session()
   request.headers.update({ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36' })
-  packages = URL
-  if packages[-1] != '/':
-    packages += '/'
-  packages += 'Packages'
-  response = request.get(packages)
-  update = response.status_code >= 400
-  if update:
-    print(packages, 'missing, force republish')
+  baseurl = URL
+  if baseurl[-1] != '/':
+    baseurl += '/'
+  for asset in ['Packages', 'install.sh']:
+    asset = baseurl + asset
+    response = request.get(packages)
+    if response.status_code >= 400:
+      print(asset, 'missing, force republish')
+      update = True
 if not update:
   sys.exit(1) # confusing, but returning an "error" here will cause the exit code to be falsish and *not* force a rebuild
 
